59ed6893c31dc675ce1b5275b51a7727b814711d
Kevinwiryansyah1 <Wiriansyahkevin@gmail.com>
2024-04-02 11:07:02 +0800

    ini commit kedua

--- app.py
+++ app.py
@@ -8,5 +8,18 @@ def hitungLuasSegitiga(a, t):
 
 alas = float(input("Input Alas Segitiga :"))
 alas = float(input("Input Tinggi Segitiga :"))
-print('luas segitiga = ', hitungLuasSegitiga(alas,Tinggi))
+print('luas segitiga = ', hitungLuasSegitiga(alas,tinggi))
 print()
+
+print("## PROGRAM PYTHON MENHITUNG PANJANG PERSEGI ##")
+print("===============================================")
+print()
+
+def hitungPersegiPanjang(p,l):
+    return round(p *1,2)
+
+panjang = float(input('input panjang : '))
+lebar = float(input('inputlebar : '))
+print('luas persegi panjang = ',hitungPersegiPanjang(panjang,lebar))
+
+
